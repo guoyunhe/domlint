4c74f00428f1f41dfca16410aff6d06f4f867df5
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2025-04-01 14:21:32 +0800

    fix: border lint

--- src/validateStyle.ts
+++ src/validateStyle.ts
@@ -24,9 +24,7 @@ export function validateStyle(
   } else if (name.endsWith('-radius') && expected === 'round') {
     const rect = elem.getBoundingClientRect();
     const value = elem.computedStyleMap().get(name) as CSSNumericValue;
-    return (
-      value && parseFloat(value?.to('px')?.toString()) <= Math.min(rect.width, rect.height) / 2
-    );
+    return value && parseFloat(value.toString()) <= Math.min(rect.width, rect.height) / 2;
   } else {
     const value = elem.computedStyleMap().get(name)?.toString();
     return value === expected;
